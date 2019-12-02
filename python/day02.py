717e77e939be2e0bef59190f01c1440392d52542
Adnan Issadeen <eddyrox1@hotmail.com>
2019-12-02 08:04:41 +0000

    Completed day 2 part 2

--- python/day02.py
+++ python/day02.py
@@ -45,6 +45,27 @@ def run_part_one():
     puzzle_input[2] = 2
     return run_program(puzzle_input)[0]
 
+def run_part_two():
+    noun = 0
+    verb = 0
+    while True:
+        puzzle_input = get_input()
+        if verb == 100:
+            noun += 1
+            verb = 0
+        if noun == 100:
+            break
+        puzzle_input[1] = noun
+        puzzle_input[2] = verb
+        program_result = run_program(puzzle_input)[0]
+        if program_result == 19690720:
+            break
+        else:
+            verb += 1
+    return 100 * noun + verb
+
 if __name__ == "__main__":
     part_one_result = run_part_one()
     print(part_one_result)
+    part_two_result = run_part_two()
+    print(part_two_result)
