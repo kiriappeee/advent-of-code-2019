a3c8267a42e3868644962683d4d44d9be4beb187
Adnan Issadeen <eddyrox1@hotmail.com>
2019-12-02 08:24:12 +0000

    Refactored day 2 for what seems will be a reused mechanism

--- python/day02.py
+++ python/day02.py
@@ -4,37 +4,37 @@ def get_input():
 
 def getNextOperationMembers(array_to_extract_next_operation_from, starting_index=0):
     if array_to_extract_next_operation_from[starting_index] == 99:
-        return [99, starting_index]
+        return {
+            'opcode': 99,
+            'parameters': [],
+            'index_of_opcode': starting_index
+        }
     elif array_to_extract_next_operation_from[starting_index] == 1 or array_to_extract_next_operation_from[starting_index] == 2:
-        operators_to_return = array_to_extract_next_operation_from[starting_index:starting_index+4]
-        operators_to_return.append(starting_index)
-        return operators_to_return
+        return {
+            'opcode': array_to_extract_next_operation_from[starting_index],
+            'parameters': array_to_extract_next_operation_from[starting_index+1:starting_index+4],
+            'index_of_opcode': starting_index
+        }
     else:
         return getNextOperationMembers(array_to_extract_next_operation_from, starting_index+1)
 
-def getNextOperationArray(complete_array, current_index):
-    next_operation_members = getNextOperationMembers(complete_array[current_index:])
-    if len(next_operation_members) < 4:
-        return (complete_array[current_index:], len(complete_array))
-    else:
-        return (complete_array[current_index:], current_index + 4)
-
 def executeOperation(operation_specification, array_to_read_from):
-    if operation_specification[0] == 1:
-        return (array_to_read_from[operation_specification[1]]
-                + array_to_read_from[operation_specification[2]])
-    elif operation_specification[0] == 2:
-        return (array_to_read_from[operation_specification[1]]
-                * array_to_read_from[operation_specification[2]])
+    parameters = operation_specification['parameters']
+    if operation_specification['opcode'] == 1:
+        return (array_to_read_from[parameters[0]]
+                + array_to_read_from[parameters[1]])
+    elif operation_specification['opcode'] == 2:
+        return (array_to_read_from[parameters[0]]
+                * array_to_read_from[parameters[1]])
 
 def run_program(full_array):
-    starting_index = 0
+    instruction_pointer = 0
     while True:
-        next_operation_members = getNextOperationMembers(full_array, starting_index)
+        next_operation_members = getNextOperationMembers(full_array, instruction_pointer)
         execution_result = executeOperation(next_operation_members, full_array)
         if execution_result:
-            full_array[next_operation_members[3]] = execution_result
-            starting_index = 4 + next_operation_members[4]
+            full_array[next_operation_members['parameters'][-1]] = execution_result
+            instruction_pointer = len(next_operation_members['parameters']) + 1 + next_operation_members['index_of_opcode']
         else:
             break
     return full_array
