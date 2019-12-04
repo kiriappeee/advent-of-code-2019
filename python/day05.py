e931a1aae65d96ed7e91fe2136968b713c60c61c
Adnan Issadeen <eddyrox1@hotmail.com>
2019-12-04 05:35:39 +0000

    Added boilerplates for day 5

---
+++ python/day05.py
@@ -0,0 +1,16 @@
+def get_input():
+    return [l for l in open('python/inputday05.txt').read().split('\n') if l.strip != '']
+
+def run_part_one():
+    puzzle_input = get_input()
+    return
+
+def run_part_two():
+    puzzle_input = get_input()
+    return
+
+if __name__ == "__main__":
+    print("== DAY 5 PART 1 ==")
+    print(run_part_one())
+    print("== DAY 5 PART 2 ==")
+    print(run_part_two())
---
+++ python/test-day05.py
@@ -0,0 +1,9 @@
+import unittest
+from . import day05
+
+class TestDay5(unittest.TestCase):
+    def test_true_is_true(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
